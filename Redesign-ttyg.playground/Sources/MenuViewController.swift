dd35fc97e147dd5ff8c6441e2bafe7c102fff82d
Ana Carolina Soares de Melo <acsmacademy@br-ufpe-15.local>
2020-04-17 12:22:46 -0300

    Adiciona animação e aprimoramento do design

---
+++ Redesign-ttyg.playground/Sources/MenuViewController.swift
@@ -0,0 +1,7 @@
+import Foundation
+import PlaygroundSupport
+import UIKit
+
+public class MenuViewController : UIViewController{
+    
+}
---
+++ TalkToYourGina/TalkToYourGina/TalkToYourGina/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  TalkToYourGina
+//
+//  Created by Ana Carolina Soares de Melo on 16/04/20.
+//  Copyright © 2020 Ana Melo. All rights reserved.
+//
+
+import Foundation
